69b0fdc68508e1368a69201c8b5d772ae7db6df3
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-05-17 16:32:50 +0100

    3. Exploring an Alternative Way of Defining Routes

--- README.md
+++ README.md
@@ -31,3 +31,13 @@
 
 1. add a new `Products.js` component inside the `pages` folder
 2. in `App.js`, add a route for the `ProductsPage` component
+
+## 3. Exploring an Alternative Way of Defining Routes
+
+1. in `App.js`, call the `createRoutesFromElements` function imported from `react-router-dom`
+2. store its returned value in a constant called `routeDefinitions`
+3. add a `<Route>` component imported from `react-router-dom` as an argument to this function
+4. inside this `<Route>` component, add your nested route with a `path` & an `element` props
+5. call the `createBrowserRouter` function & pass the `routeDefinitions` constant to it as an argument
+6. store the returned value of this function to a constant named `router`
+7. pass this constant as a value to the `router` prop of the `<RouterProvider>` component
--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />
-  },
-  {
-    path: '/products',
-    element: <ProductsPage />
-  }
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />
+//   },
+//   {
+//     path: '/products',
+//     element: <ProductsPage />
+//   }
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
